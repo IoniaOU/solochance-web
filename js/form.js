e3bd599439d13d95cef71dd4cc545749d42c0c67
mustafatufan <mustafa@tufan.ee>
2024-02-23 23:46:25 +0300

    first-working-commit

--- js/form.js
+++ js/form.js
@@ -9,9 +9,32 @@ async function calculateHashrate() {
         const response = await fetch(apiUrl);
         const data = await response.json();
 
-        const resultContainer = document.getElementById('result');
-        resultContainer.innerHTML = `<p>API Response:</p>
-                                 <pre>${JSON.stringify(data, null, 2)}</pre>`;
+        const yourHashrateContainer = document.getElementById('your-hashrate');
+        yourHashrateContainer.innerHTML = data['currentHashrateText'];
+
+        const networkHashrateContainer = document.getElementById('network-hashrate');
+        networkHashrateContainer.innerHTML = data['networkHashrateText'];
+
+        const blockIntervalContainer = document.getElementById('block-interval');
+        blockIntervalContainer.innerHTML = data['blockIntervalInMinutes'] + ' minutes';
+
+        const blockChance = document.getElementById('chance-per-block');
+        blockChance.innerHTML = data['blockChanceText'];
+
+        const hourChance = document.getElementById('chance-per-hour');
+        hourChance.innerHTML = data['hourChanceText'];
+
+        const dayChance = document.getElementById('chance-per-day');
+        dayChance.innerHTML = data['dayChanceText'];
+
+        const weekChance = document.getElementById('chance-per-week');
+        weekChance.innerHTML = data['weekChanceText'];
+
+        const monthChance = document.getElementById('chance-per-month');
+        monthChance.innerHTML = data['monthChanceText'];
+
+        const yearChance = document.getElementById('chance-per-year');
+        yearChance.innerHTML = data['yearChanceText'];
     } catch (error) {
         console.error('Error fetching data:', error);
     }
